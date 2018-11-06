9dc2763aee343b022246c746368298e37dee99a6
Schwim Dandy <github@schwim.net>
2018-11-06 14:14:11 -0500

    Upstream fix

--- [sesx]/es_extended/__resource.lua
+++ [sesx]/es_extended/__resource.lua
@@ -1,4 +1,4 @@
-resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ES Extended'
 
@@ -7,20 +7,29 @@ version '1.0.14'
 server_scripts {
 	'@async/async.lua',
 	'@mysql-async/lib/MySQL.lua',
+
 	'locale.lua',
 	'locales/de.lua',
 	'locales/br.lua',
 	'locales/fr.lua',
 	'locales/en.lua',
+	'locales/fi.lua',
 	'locales/sv.lua',
+	'locales/pl.lua',
+	'locales/cs.lua',
+
 	'config.lua',
 	'config.weapons.lua',
+
 	'server/common.lua',
 	'server/classes/player.lua',
 	'server/functions.lua',
 	'server/paycheck.lua',
 	'server/main.lua',
-	'server/commands.lua'
+	'server/commands.lua',
+
+	'shared/modules/math.lua',
+	'shared/functions.lua'
 }
 
 client_scripts {
@@ -29,14 +38,25 @@ client_scripts {
 	'locales/br.lua',
 	'locales/fr.lua',
 	'locales/en.lua',
+	'locales/fi.lua',
 	'locales/sv.lua',
+	'locales/pl.lua',
+	'locales/cs.lua',
+
 	'config.lua',
 	'config.weapons.lua',
+
 	'client/common.lua',
 	'client/entityiter.lua',
 	'client/functions.lua',
 	'client/wrapper.lua',
-	'client/main.lua'
+	'client/main.lua',
+
+	'client/modules/scaleform.lua',
+	'client/modules/streaming.lua',
+
+	'shared/modules/math.lua',
+	'shared/functions.lua'
 }
 
 ui_page {
@@ -44,6 +64,7 @@ ui_page {
 }
 
 files {
+	'locale.js',
 	'html/ui.html',
 
 	'html/css/app.css',
@@ -55,7 +76,6 @@ files {
 	'html/fonts/pdown.ttf',
 	'html/fonts/bankgothic.ttf',
 
-	'html/img/cursor.png',
 	'html/img/keys/enter.png',
 	'html/img/keys/return.png',
 
